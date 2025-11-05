cddb5cdd9cfa330d35bd8386099d51d7f4f6a0c7
zablonzekky <zablonzekky@gmail.com>
2025-11-05 12:57:44 +0300

    Ui change

---
+++ customer/pay_offline.php
@@ -0,0 +1,40 @@
+<center>
+	<h1>  Pay offline using method</h1>
+	<p class="lead"> Pay offline using method </p>
+	<p class="text-muted"> If you have any questions, feel free to <a href="../contact.php">Contact Us</a>. 
+		Our Customer Service works <strong>24/7</strong>
+	</p>
+</center>
+
+<hr>
+<div class="table-responsive"><!-- table-responsive Begin -->
+	
+	<table class="table table-bordered table-hover table-striped">
+
+		<thead>
+			<tr>
+				<th>Bank Account details: </th>
+				<th> Easy paisa,Ubl OMNL,Mobi Cash Details: </th>
+				<th> Western Union : </th>
+				
+			</tr>
+		</thead>
+
+		<tbody>
+			<td>
+Bank name | Account NO:12689|Branch name|Kenya|Branch code| 1914
+            </td>
+           
+			<td>
+            NIC #10191 |Mobile no:46976573 |Name:Ezekiel
+                
+            </td>
+            <td>
+            Real name:Mr Wekesa |Mobile no:11233445 |Country : Kenya|Name:Wekesa|Nic:1223
+
+            </td>
+		</tbody>
+		
+	</table>
+
+</div><!-- table-responsive Finish -->
